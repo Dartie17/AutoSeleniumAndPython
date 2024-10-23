831b0e3aa82479ca4fb3a3414af32f2e14ec3016
Dartie <kinskyalex@gmail.com>
2024-10-23 22:21:04 +0300

    Added lesson 3.2.8

---
+++ Chapter 3/lesson 3.2.8.py
@@ -0,0 +1,7 @@
+def test_input_text(expected_result, actual_result):
+    assert expected_result == actual_result, \
+        f"expected {expected_result}, got {actual_result}"
+
+a, b = int(input()), int(input())
+
+test_input_text(a, b)
