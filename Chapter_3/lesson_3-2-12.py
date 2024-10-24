40de8a87d73c2cb388ac9883300372545090947c
Dartie <kinskyalex@gmail.com>
2024-10-24 23:01:46 +0300

    Add lesson 3.2.12 and refactor names

---
+++ Chapter_3/lesson_3-2-12.py
@@ -0,0 +1,53 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+class TestWeb(unittest.TestCase):
+
+    def test_web1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        input1 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your first name']")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your last name']")
+        input2.send_keys("Petrov")
+        input3 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your email']")
+        input3.send_keys("petrov_i@mail.test")
+
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        time.sleep(1)
+
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+
+        self.assertEqual(welcome_text, "Congratulations! You have successfully registered!", "Should be correct message")
+
+    def test_web2(self):
+        link = "https://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        input1 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your first name']")
+        input1.send_keys("Ivan")
+        input2 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your last name']")
+        input2.send_keys("Petrov")
+        input3 = browser.find_element(By.CSS_SELECTOR, "[placeholder='Input your email']")
+        input3.send_keys("petrov_i@mail.test")
+
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        time.sleep(1)
+
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+
+        self.assertEqual(welcome_text, "Congratulations! You have successfully registered!", "Should be correct message")
+
+if __name__ == "__main__":
+    unittest.main()
